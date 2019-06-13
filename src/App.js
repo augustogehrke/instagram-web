93c2c585c54130face5caf979b25dc73bb26212c
Augusto Gehrke <augusto.gehrke@sistemairriga.com.br>
2019-06-13 14:41:57 -0300

    criação de rotas e tela inicial para buscar as imagens

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Routes from './routes';
+import {BrowserRouter} from 'react-router-dom';
+import Header from './components/Header';
 import './global.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>OLá</h1>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes/>
+    </BrowserRouter>
   );
 }
 
